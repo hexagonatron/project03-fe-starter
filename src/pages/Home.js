c586113c36db574c289f32198998379c0fe6b73d
Ben Fawcett <ben.fawcett@nqry.com.au>
2021-08-10 18:22:12 +0930

    fixing input

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,31 @@
 import { useMutation } from '@apollo/client';
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import { UPLOAD_FILE } from '../api/mutations';
 
 const Home = () => {
   const [mutate ] = useMutation(UPLOAD_FILE)
   const [file, setFile] = useState(null);
+  const inputRef = useRef(null);
   const submit = async () => {
-    console.log(`Uploading ${file.filename}.`)
+    if (!file) {
+      return
+    }
+    console.log(`Uploading ${file.name}.`)
     const fileResponse = await mutate({variables: {file}});
-    console.log(` Sucessfully uploaded ${fileResponse.data.filename}`);
+    console.log(`Sucessfully uploaded ${fileResponse.data.singleUpload.filename}`);
     setFile(null);
+    inputRef.current.value = "";
   }
+
+  const updateFile = ({target: {files:[inputFile]}}) => {
+    setFile(inputFile || null);
+    console.log(inputFile.name)
+  }
+
   return (
     <div className="container">
       <h1>Home</h1>
-      <input type="file" onChange={({target: {files: [inputFile]}}) => setFile(inputFile)} />
+      <input ref={inputRef} type="file" onChange={updateFile} />
       <button onClick={submit}>Submit</button>
     </div>
   );
